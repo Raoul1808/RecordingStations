8d716fc001c83a7f3979c1c55a78d3deb1d09802
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-09-24 13:22:40 +0200

    Initial Commit

---
+++ RecordingStationsModule.cs
@@ -0,0 +1,24 @@
+﻿using Celeste.Mod;
+
+namespace RecordingStations
+{
+    public class RecordingStationsModule : EverestModule
+    {
+        public static RecordingStationsModule Instance;
+
+        public RecordingStationsModule()
+        {
+            Instance = this;
+        }
+        
+        public override void Load()
+        {
+            Logger.Log("RecStation", "Hello from RecordingStationsModule!");
+        }
+
+        public override void Unload()
+        {
+            Logger.Log("RecStation", "Goodbye from RecordingStationsModule!");
+        }
+    }
+}
